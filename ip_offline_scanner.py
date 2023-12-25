f751c0d825a6b30107d269540611ebada8e3e8b4
ChrononX <45243151+ChrononX@users.noreply.github.com>
2023-12-25 22:18:03 +0400

    Update ip_offline_scanner.py

--- ip_offline_scanner.py
+++ ip_offline_scanner.py
@@ -33,6 +33,20 @@ def get_ip_info(target, city_reader, country_reader, asn_reader):
     except geoip2.errors.AddressNotFoundError:
         return "Information not available", "Unknown", "Unknown", "Unknown"
 
+def process_log_file(log_file_path, city_reader, country_reader, asn_reader):
+    try:
+        with open(log_file_path, 'r') as log_file:
+            for line in log_file:
+                # Split each line into words and check for IP addresses or domains
+                words = line.split()
+                for word in words:
+                    ip_info = get_ip_info(word, city_reader, country_reader, asn_reader)
+                    print(f"\nInformation for {ip_info['Target']} ({ip_info['IP Address']}):")
+                    for key, value in ip_info.items():
+                        print(f"{key}: {value}")
+    except FileNotFoundError:
+        print(f"Error: Log file not found at {log_file_path}")
+
 def main():
     script_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -45,15 +59,21 @@ def main():
             geoip2.database.Reader(country_database_path) as country_reader:
 
         while True:
-            target = input("Enter an IP address or domain name (or 'exit' to quit): ")
-            if target.lower() == 'exit':
+            option = input("Enter '1' to enter IP address or domain directly, '2' to process a logfile, or 'exit' to quit: ")
+            
+            if option.lower() == 'exit':
                 break
 
-            ip_info = get_ip_info(target, city_reader, country_reader, asn_reader)
+            if option == '1':
+                target = input("Enter an IP address or domain name: ")
+                ip_info = get_ip_info(target, city_reader, country_reader, asn_reader)
+                print(f"\nInformation for {ip_info['Target']} ({ip_info['IP Address']}):")
+                for key, value in ip_info.items():
+                    print(f"{key}: {value}")
 
-            print(f"\nInformation for {ip_info['Target']} ({ip_info['IP Address']}):")
-            for key, value in ip_info.items():
-                print(f"{key}: {value}")
+            elif option == '2':
+                log_file_path = input("Enter the path to the logfile: ")
+                process_log_file(log_file_path, city_reader, country_reader, asn_reader)
 
 if __name__ == "__main__":
     main()
